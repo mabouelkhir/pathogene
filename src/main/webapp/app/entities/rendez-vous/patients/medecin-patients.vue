b6f0e75939d408ac5be8b15a840ea2e7095c4ded
mabouelkhir <101573892+mabouelkhir@users.noreply.github.com>
2024-01-04 16:57:17 +0100

    Update medecin-patients.vue

--- src/main/webapp/app/entities/rendez-vous/patients/medecin-patients.vue
+++ src/main/webapp/app/entities/rendez-vous/patients/medecin-patients.vue
@@ -63,7 +63,7 @@
                 <a v-if="patient.photo" v-on:click="openFile(patient.photoContentType, patient.photo)">
                   <img
                     v-bind:src="'data:' + patient.photoContentType + ';base64,' + patient.photo"
-                    style="max-height: 70px"
+                    style="max-height: 70px; width: 60px"
                     alt="patient image"
                   />
                 </a>
@@ -76,16 +76,6 @@
                       <span class="d-none d-md-inline" v-text="$t('entity.action.view')">View</span>
                     </button>
                   </router-link>
-                  <b-button
-                    v-on:click="prepareDetection(patient)"
-                    variant="success"
-                    class="btn btn-sm mr-1"
-                    data-cy="entityDetectionButton"
-                    v-b-modal.detectionEntity
-                  >
-                    <font-awesome-icon icon="plus"></font-awesome-icon>
-                    <span class="d-none d-md-inline">Detection</span>
-                  </b-button>
                   <b-button
                     v-on:click="prepareStade(patient)"
                     variant="dark"
@@ -102,95 +92,6 @@
           </tbody>
         </table>
       </div>
-      <b-modal ref="detectionEntity" id="detectionEntity">
-        <span slot="modal-title"
-          ><span id="pathogeneApp.patient.detection.question" data-cy="patientDetectionDialogHeading">Create detection</span></span
-        >
-        <div class="modal-body">
-          <div class="form-group">
-            <label class="form-control-label" for="detection-photo">Photo</label>
-            <div>
-              <img
-                v-bind:src="'data:' + detection.photoContentType + ';base64,' + detection.photo"
-                style="max-height: 100px"
-                v-if="detection.photo"
-                alt="detection image"
-              />
-              <div v-if="detection.photo" class="form-text text-danger clearfix">
-                <span class="pull-left">{{ detection.photoContentType }}, {{ byteSize(detection.photo) }}</span>
-                <button
-                  type="button"
-                  v-on:click="clearInputImage(detection, 'file_photo', 'photo', 'photoContentType', 'file_photo')"
-                  class="btn btn-secondary btn-xs pull-right"
-                >
-                  <font-awesome-icon icon="times"></font-awesome-icon>
-                </button>
-              </div>
-              <input
-                type="file"
-                ref="file_photo"
-                id="file_photo"
-                data-cy="photo"
-                v-on:change="setFileData($event, detection, 'photo', true)"
-                accept="image/*"
-              />
-            </div>
-            <input
-              type="hidden"
-              class="form-control"
-              name="photo"
-              id="detection-photo"
-              data-cy="photo"
-              :class="{ valid: !$v.detection.photo.$invalid, invalid: $v.detection.photo.$invalid }"
-              v-model="$v.detection.photo.$model"
-            />
-            <input
-              type="hidden"
-              class="form-control"
-              name="photoContentType"
-              id="detection-photoContentType"
-              v-model="detection.photoContentType"
-            />
-          </div>
-
-          <div class="form-group">
-            <label class="form-control-label" for="detection-validation">Validation</label>
-            <input
-              type="checkbox"
-              class="form-check"
-              name="validation"
-              id="detection-validation"
-              data-cy="validation"
-              :class="{ valid: !$v.detection.validation.$invalid, invalid: $v.detection.validation.$invalid }"
-              v-model="$v.detection.validation.$model"
-            />
-          </div>
-
-          <div class="form-group">
-            <label class="form-control-label">Visit</label>
-            <select class="form-control" id="visite-patient" data-cy="visite" name="patient" v-model="idVisite">
-              <option v-for="visite in visites" :key="visite.id" v-bind:value="visite.id">
-                {{ visite.date }}
-              </option>
-            </select>
-          </div>
-
-          <div class="form-group">
-            <label class="form-control-label">Disease</label>
-            <select class="form-control" id="maladie-patient" data-cy="maladie" name="maladie" v-model="idMaladie">
-              <option v-for="maladie in maladies" :key="maladie.id" v-bind:value="maladie.id">
-                {{ maladie.nom }}
-              </option>
-            </select>
-          </div>
-        </div>
-        <div slot="modal-footer">
-          <button type="button" class="btn btn-secondary" v-on:click="closeDialog()">Cancel</button>
-          <button type="submit" id="save-entity" data-cy="entityCreateSaveButton" class="btn btn-primary" v-on:click="saveDetection()">
-            <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span>Save</span>
-          </button>
-        </div>
-      </b-modal>
 
       <b-modal ref="stadeEntity" id="stadeEntity">
         <span slot="modal-title"
@@ -227,44 +128,6 @@
         </div>
       </b-modal>
 
-      <b-modal ref="afficheEntity" id="afficheEntity">
-        <span slot="modal-title"
-          ><span id="pathogeneApp.detection.affiche.question" data-cy="detectionAfficheDialogHeading">Result AI</span></span
-        >
-        <div class="modal-body">
-          <dl class="row jh-entity-details">
-            <dt>
-              <span>Photo</span>
-            </dt>
-            <dd>
-              <div v-if="detection.photo">
-                <a v-on:click="openFile(detection.photoContentType, detection.photo)">
-                  <img
-                    v-bind:src="'data:' + detection.photoContentType + ';base64,' + detection.photo"
-                    style="width: 90px; max-height: 70px"
-                    alt="patient image"
-                  />
-                </a>
-              </div>
-            </dd>
-            <dt>
-              <span>Description</span>
-            </dt>
-            <dd>
-              <span>{{ detection.description }}</span>
-            </dd>
-            <dt>
-              <span>Stage</span>
-            </dt>
-            <dd>
-              <span>{{ detection.stade }}</span>
-            </dd>
-          </dl>
-        </div>
-        <div slot="modal-footer">
-          <button type="button" class="btn btn-secondary" v-on:click="closeDialog()">Leave</button>
-        </div>
-      </b-modal>
       <div v-show="patients && patients.length > 0">
         <div class="row justify-content-center">
           <jhi-item-count :page="page" :total="queryCount" :itemsPerPage="itemsPerPage"></jhi-item-count>
